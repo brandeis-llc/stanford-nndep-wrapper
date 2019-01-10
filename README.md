0eeea21595aa10a5f68952066d25aad394a8f05a
Keigh Rim <keigh.rim@gmail.com>
2019-01-09 22:04:05 -0500

    added an actual CLI argument structure

--- README.md
+++ README.md
@@ -4,17 +4,23 @@ A simple wrapper around stanford dependency parser.
 
 ## Usage: 
 ```
-This program wraps around stanford neural dependency parser. 
-	stanford parser version: corenlp 3.9.2
+This program wraps around stanford neural dependency parser.
+        stanford parser version: coreNLP 3.9.2
 
-	Currently only supports input through STDIN and output to STDOUT
+usage:
+ -d,--delim <arg>    input annotation delimiter
+                     MUST be a single char (defaults to '/')
+ -h,--help <arg>     print this message
+ -i,--input <arg>    input file name (defaults to STDIN)
+                     Existing tokenization (with whitespace) is preserved.
+                     Additionally POS or lemma+POS can be passed
+                     (see "delim" option)
+                     e.g. Karen/NNP flew/VBP to/TO New_York/NNP ./PUNC
+                     e.g. Karen/Karen/NNP flew/fly/VBP to/to/TO
+                     New_York/New_York/NNP ././PUNC
+ -o,--output <arg>   output file name (defaults to STDOUT)
+                     CoNLL-X format will be used for dependency annotation
+                     All runtime messages go to STDERR
 
-	INPUT:  Pass a sentence line by line via STDIN
-	        Existing tokenization (with whitespace) is always preserved. 
-	        Additionally POS or lemma+POS can be passed - use '/' as delimiter. 
-	        e.g. Karen/NNP flew/VBP to/TO New_York/NNP ./PUNC
-	        e.g. Karen/Karen/NNP flew/fly/VBP to/to/TO New_York/New_York/NNP ././PUNC
-	OUTPUT: CoNLL-X format dependency annotation is printed to STDOUT
-	        All runtime messages go to STDERR
 ```
 
--- src/main/java/edu/brandeis/llc/stanford_nndep_wrapper/Main.java
+++ src/main/java/edu/brandeis/llc/stanford_nndep_wrapper/Main.java
@@ -5,7 +5,9 @@ import edu.stanford.nlp.parser.nndep.DependencyParser;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.TypedDependency;
+import org.apache.commons.cli.*;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -13,13 +15,42 @@ import java.util.Scanner;
 
 public class Main {
 
-    static DependencyParser p = new DependencyParser(new Properties());
+    private static String ANNOTATION_DELIMITER;
+    private static String TOKEN_DELIMITER = " ";
+    private static DependencyParser p = new DependencyParser(new Properties());
+
+
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            help();
-            System.exit(0);
+
+        Options options = new Options();
+        Option help = new Option("h", "help", true, "print this message");
+        Option inFile = new Option("i", "input", true, "input file name (defaults to STDIN)\n" +
+                "Existing tokenization (with whitespace) is preserved.\n"+
+                "Additionally POS or lemma+POS can be passed\n" +
+                "(see \"delim\" option)\n" +
+                "e.g. Karen/NNP flew/VBP to/TO New_York/NNP ./PUNC\n" +
+                "e.g. Karen/Karen/NNP flew/fly/VBP to/to/TO New_York/New_York/NNP ././PUNC");
+        Option outFile = new Option("o", "output", true, "output file name (defaults to STDOUT)\n" +
+                "CoNLL-X format will be used for dependency annotation\n" +
+                "All runtime messages go to STDERR");
+        Option delimiter = new Option("d", "delim", true, "input annotation delimiter \nMUST be a single char (defaults to '/')");
+        options.addOption(help);
+        options.addOption(inFile);
+        options.addOption(outFile);
+        options.addOption(delimiter);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine argv = null;
+        try {
+            argv = parser.parse(options, args);
+        } catch (ParseException e) {
+            help(options);
         }
+
+        String infile = argv.hasOption('i') ? argv.getOptionValue('i') : null;
+        String outfile = argv.hasOption('o') ? argv.getOptionValue('o') : null;
+        ANNOTATION_DELIMITER = argv.hasOption('d') && argv.getOptionValue('d').length() == 1 ? argv.getOptionValue('d') : "/";
 
         // quick sanity check
 //        String sample = "The/DT Fulton_County_Grand_Jury/NNP said/VB Friday/NN an/DT investigation/NN of/IN Atlanta/NN 's/POS recent/JJ primary_election/NN produced/VB `/` no/DT evidence/NN '/' that/IN any/DT irregularities/NN took_place/VB ./.";
@@ -27,17 +58,50 @@ public class Main {
 //                "\nThe/the/DT Fulton_County_Grand_Jury/fcgj/NNP said/say/VB Friday/friday/NN an/a/DT investigation/investigae/NN of/of/IN Atlanta/atlanta/NN 's/s/POS recent/recent/JJ primary_election/pe/NN produced/produce/VB `/`/` no/no/DT evidence/evidence/NN '/'/' that/that/IN any/a/DT irregularities/irregularity/NN took_place/take_place/VB ./.";
 //        Scanner input = new Scanner(sample);
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = null;
+        if (infile == null) {
+            input = new Scanner(System.in);
+            System.err.println("accepting input from STDIN");
+        } else {
+            try {
+                input = new Scanner(new File(infile));
+            } catch (FileNotFoundException e) {
+                error("file not found: " + infile);
+            }
+        }
+        PrintStream out = null;
+        if (outfile == null) {
+            out = new PrintStream(System.out);
+        } else {
+            File outf = new File(outfile);
+            if (!outf.exists()) {
+                try {
+                    outf.createNewFile();
+                } catch (IOException e) {
+                    error("could not create the output file: " + outfile);
+                }
+            }
+            try {
+                out = new PrintStream(new FileOutputStream(outf));
+            } catch (FileNotFoundException ignored) {
+            }
+        }
+
+//        Scanner input = new Scanner(infile == null ? System.in : new File(infile));
         int sentIdx = 1;
         p.loadModelFile("english_UD.gz");
+        if (input == null) {
+            error("input source is not found");
+            return;
+        }
         while (input.hasNext()) {
             String inputLine = input.nextLine();
-            String[] inputTokens = inputLine.split(" ");
+            String[] inputTokens = inputLine.split(TOKEN_DELIMITER);
             CoreLabelTokenFactory tf = new CoreLabelTokenFactory(false);
             List<CoreLabel> sentenceTokens = new ArrayList<>();
-            String originalSentence = "";
+            StringBuilder originalSentence = new StringBuilder();
             for (String token : inputTokens) {
-                String[] tsplit = token.split("/");
+                String[] tsplit = token.split(ANNOTATION_DELIMITER);
                 String word = null;
                 String pos = null;
                 String lemma = null;
@@ -51,7 +115,7 @@ public class Main {
                 } else {
                     error("unknown input format");
                 }
-                originalSentence = originalSentence + word + " ";
+                originalSentence.append(word).append(" ");
                 CoreLabel coreToken = tf.makeToken(word, 0, 0);
                 coreToken.setTag(pos);
                 if (lemma != null) {
@@ -61,9 +125,9 @@ public class Main {
             }
 
             // leave comments of sentence metadata
-            System.out.printf("# sent = %d\n# text = %s\n",
+            out.printf("# sent = %d\n# text = %s\n",
                     sentIdx,
-                    originalSentence
+                    originalSentence.toString()
             );
 
             GrammaticalStructure tree = p.predict(sentenceTokens);
@@ -72,7 +136,7 @@ public class Main {
                 CoreLabel token = sentenceTokens.get(tokenIdx - 1);
                 if (tokenIdx == rel.dep().index()) {
 
-                    System.out.printf("%d\t%s\t%s\t%s\t%s\t_\t%d\t%s\t_\t_%n",
+                    out.printf("%d\t%s\t%s\t%s\t%s\t_\t%d\t%s\t_\t_%n",
                             tokenIdx,
                             token.word(),
                             token.lemma() != null ? token.lemma() : "_",
@@ -85,29 +149,23 @@ public class Main {
                 }
                 tokenIdx++;
             }
-            System.out.println();
+            out.println();
             sentIdx++;
         }
     }
 
     private static void error(String msg) {
         System.err.println(msg);
+        System.exit(1);
     }
 
-    private static void help() {
+    private static void help(Options options) {
         System.out.println("This program wraps around stanford neural dependency parser. ");
-        System.out.println("\tstanford parser version: corenlp 3.9.2");
+        System.out.println("\tstanford parser version: coreNLP 3.9.2");
         System.out.println();
-        System.out.println("\tCurrently only supports input through STDIN and output to STDOUT");
-        System.out.println();
-        System.out.println("\tINPUT:  Pass a sentence line by line via STDIN");
-        System.out.println("\t        Existing tokenization (with whitespace) is always preserved. ");
-        System.out.println("\t        Additionally POS or lemma+POS can be passed - use '/' as delimiter. ");
-        System.out.println("\t        e.g. Karen/NNP flew/VBP to/TO New_York/NNP ./PUNC");
-        System.out.println("\t        e.g. Karen/Karen/NNP flew/fly/VBP to/to/TO New_York/New_York/NNP ././PUNC");
-        System.out.println("\tOUTPUT: CoNLL-X format dependency annotation is printed to STDOUT");
-        System.out.println("\t        All runtime messages go to STDERR");
-        System.out.println();
+        HelpFormatter helper = new HelpFormatter();
+        helper.printHelp(" ", options);
+        System.exit(0);
     }
 }
 
